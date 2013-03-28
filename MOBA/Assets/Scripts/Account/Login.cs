af3f37f90cf758ff127e0a38eedfdce1fee800bd
Christopher Dinh <christod@uci.edu>
2013-03-27 19:56:51 -0700

    Implemented login

--- MOBA/Assets/Scripts/Account/Login.cs
+++ MOBA/Assets/Scripts/Account/Login.cs
@@ -3,16 +3,19 @@ using System.Collections;
 
 public class Login : MonoBehaviour {
 
-    private const string LOGIN_URL = "";
-    private const string REGISTER_URL = "";
+    private const string LOGIN_URL = "http://mobagame.christopherdinh.com/login.php";
+    private const string REGISTER_URL = "http://mobagame.christopherdinh.com/register.php";
     private const string APP_CODE = "68c00e32fccadee9787f78156c2a0432";
+
+    private const string DELIMITER = "||";
 
     private bool m_IsLoggingIn;
 
     private string m_Username;
     private string m_Password;
 
-    private GUIStyle m_GuiStyle;
+    public GUIStyle TextStyle;
+    public GUIStyle ButtonStyle;
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +23,6 @@ public class Login : MonoBehaviour {
 
         m_Username = "";
         m_Password = "";
-
-        m_GuiStyle = new GUIStyle();
 	}
 	
 	// Update is called once per frame
@@ -36,16 +37,18 @@ public class Login : MonoBehaviour {
 
         }
 
-        GUI.Label(GuiUtility.PercRect(0.4f, 0.25f, 0.2f, 0.05f), "Username");
+        GUI.Label(GuiUtility.PercRect(0.4f, 0.25f, 0.2f, 0.05f), "Username", TextStyle);
         m_Username = GUI.TextField(GuiUtility.PercRect(0.4f, 0.3f, 0.2f, 0.05f), m_Username, 32);
 
-        GUI.Label(GuiUtility.PercRect(0.4f, 0.36f, 0.2f, 0.05f), "Password");
+        GUI.Label(GuiUtility.PercRect(0.4f, 0.36f, 0.2f, 0.05f), "Password", TextStyle);
         m_Password = GUI.PasswordField(GuiUtility.PercRect(0.4f, 0.40f, 0.2f, 0.05f), m_Password, '*', 32);
 
         if (GUI.Button(GuiUtility.PercRect(0.45f, 0.5f, 0.1f, 0.05f), "Login"))
         {
-            audio.Play();
-            SubmitLogin();
+            if (m_Username.Length != 0 && m_Password.Length != 0)
+            {
+                StartCoroutine(SubmitLogin());
+            }
         }
     }
 
@@ -57,6 +60,9 @@ public class Login : MonoBehaviour {
     {
     }
 
+    // Test login:
+    // User: testUser
+    // Password: testPassword
     private IEnumerator SubmitLogin()
     {
         WWWForm form = new WWWForm();
@@ -71,11 +77,21 @@ public class Login : MonoBehaviour {
 
         if (w.error == null)
         {
-            Debug.Log(w.text);
-            Application.LoadLevel("MainMenu");
+            Debug.Log("Login successful!");
+            string response = w.text;
+            Debug.Log(response);
+
+            int sessionLength = response.IndexOf(DELIMITER);
+            string sessionID = response.Substring(0, sessionLength);
+            string token = response.Substring(sessionLength + DELIMITER.Length);
+
+            Debug.Log("Session ID: " + sessionID);
+            Debug.Log("Token: " + token);
+            //Application.LoadLevel("MainMenu");
         }
         else
         {
+            Debug.Log("Error");
             Debug.Log(w.text);
         }
 
